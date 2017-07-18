65f1e1c41c644552adb50b2389fced503f01e58e
BigMarbz <matthewwang2020@u.northwestern.edu>
2017-07-18 13:18:11 -0500

    BLE Activities

--- app/src/main/java/orihd/orihd/FragmentTab3.java
+++ app/src/main/java/orihd/orihd/FragmentTab3.java
@@ -17,27 +17,11 @@ public class FragmentTab3 extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        mcontext.getApplicationContext();
+
         View rootView = inflater.inflate(R.layout.activity_fragment_tab3, container, false);
-        // Initializes Bluetooth adapter.
-        final BluetoothManager bluetoothManager =
-                (BluetoothManager) mcontext.getSystemService(Context.BLUETOOTH_SERVICE);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
-            mBluetoothAdapter = bluetoothManager.getAdapter();
-        }
-        // Ensures Bluetooth is available on the device and it is enabled. If not,
-        // displays a dialog requesting user permission to enable Bluetooth.
-        if (mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled()) {
-            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            int REQUEST_ENABLE_BT=1;
-            //The REQUEST_ENABLE_BT constant passed to
-            // startActivityForResult(android.content.Intent, int)
-            // is a locally-defined integer (which must be greater than 0)
-            // that the system passes back to you in your
-            // onActivityResult(int, int, android.content.Intent)
-            // implementation as the requestCode parameter.
-            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-        }
+
+
+
         return rootView;
     }
 
