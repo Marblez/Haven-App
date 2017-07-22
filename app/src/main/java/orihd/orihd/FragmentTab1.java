0cf645dfe3cd4381e256ee657996a72c1c573ea4
BigMarbz <matthewwang2020@u.northwestern.edu>
2017-07-22 13:44:45 -0500

    Clustering

--- app/src/main/java/orihd/orihd/FragmentTab1.java
+++ app/src/main/java/orihd/orihd/FragmentTab1.java
@@ -74,7 +74,7 @@ public class FragmentTab1 extends Fragment {
         BluetoothManager mBluetoothManager = MainActivity.bmstatic;
         BluetoothAdapter mBluetoothAdapter = MainActivity.bmadapter;
         BTLE_Device device = MainActivity.btlestatic;
-
+        boolean ans = mBluetoothGatt.discoverServices();
 
         //BluetoothGattService service = new BluetoothGattService();
         // BluetoothGattCharacteristic battery = (BluetoothGattCharacteristic) service.getCharacteristic(UUID
@@ -183,6 +183,16 @@ public class FragmentTab1 extends Fragment {
     return rootview;
     }
 
+    public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+
+        if (status == BluetoothGatt.GATT_SUCCESS) {
+
+        }
+        else {
+            Toast.makeText(getApplicationContext(), "No Atts", Toast.LENGTH_SHORT).show();
+
+        }
+    }
 
     public void disconnect() {
         if (mBluetoothGatt == null) {
@@ -225,7 +235,57 @@ public class FragmentTab1 extends Fragment {
     String fullname = firstName + " " + lastName;
     String facebook_id = profile.getId();
 
+    private void displayGattServices(List<BluetoothGattService> gattServices) {
+        if (gattServices == null) return;
+        String uuid = null;
+        String unknownServiceString = getResources().
+                getString(R.string.unknown_service);
+        String unknownCharaString = getResources().
+                getString(R.string.unknown_characteristic);
+        ArrayList<HashMap<String, String>> gattServiceData =
+                new ArrayList<HashMap<String, String>>();
+        ArrayList<ArrayList<HashMap<String, String>>> gattCharacteristicData
+                = new ArrayList<ArrayList<HashMap<String, String>>>();
+        ArrayList<ArrayList<BluetoothGattCharacteristic>> mGattCharacteristics =
+                new ArrayList<ArrayList<BluetoothGattCharacteristic>>();
 
+        // Loops through available GATT Services.
+        for (BluetoothGattService gattService : gattServices) {
+            HashMap<String, String> currentServiceData =
+                    new HashMap<String, String>();
+            uuid = gattService.getUuid().toString();
+            /*
+            currentServiceData.put(
+                    LIST_NAME, SampleGattAttributes.
+                            lookup(uuid, unknownServiceString));
+            currentServiceData.put(LIST_UUID, uuid);
+            gattServiceData.add(currentServiceData);
+*/ 
+            ArrayList<HashMap<String, String>> gattCharacteristicGroupData =
+                    new ArrayList<HashMap<String, String>>();
+            List<BluetoothGattCharacteristic> gattCharacteristics =
+                    gattService.getCharacteristics();
+            ArrayList<BluetoothGattCharacteristic> charas =
+                    new ArrayList<BluetoothGattCharacteristic>();
+            // Loops through available Characteristics.
+            for (BluetoothGattCharacteristic gattCharacteristic :
+                    gattCharacteristics) {
+                charas.add(gattCharacteristic);
+                HashMap<String, String> currentCharaData =
+                        new HashMap<String, String>();
+                uuid = gattCharacteristic.getUuid().toString();
+            /*
+                currentCharaData.put(
+                        LIST_NAME, SampleGattAttributes.lookup(uuid,
+                                unknownCharaString));
+                currentCharaData.put(LIST_UUID, uuid);
+                gattCharacteristicGroupData.add(currentCharaData);
+                */
+            }
+            mGattCharacteristics.add(charas);
+            gattCharacteristicData.add(gattCharacteristicGroupData);
+        }
+    }
 
 
 
